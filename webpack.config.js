122ec09280dcc2480a5a4a772b8fbf13e4b41919
marwa youssef <marwayoussef678@gmail.com>
2025-04-13 23:28:21 +0200

    install webpack and webpack-cli

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const webpack = require ("webpack")
+ const path = require("path")
+
+
+
+
+module.exports ={
+  entry:"./src/client/index.js",
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env"]
+        }
+      }
+    }
+  ]
+}
