01c62ac96d001dd4ad50337c3f2aa82051e4bdf7
marwa youssef <marwayoussef678@gmail.com>
2025-04-21 03:20:35 +0200

    Add and update project files including README.md

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,21 @@
-alert("I Exist");
+// JS files
+import { handleSubmit } from './js/formHandler';
+
+// Attach the event listener for the form
+document.getElementById('urlForm').addEventListener('submit', handleSubmit);
+
+// SCSS file 
+import './styles/main.scss';
+
+//service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch((error) => {
+        console.log('Service Worker registration failed:', error);
+      });
+  });
+}
